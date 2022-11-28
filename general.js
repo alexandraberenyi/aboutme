9d67f807f0048a31986f721579e22b5a2ecae225
alexandraberenyi <alexandra.berenyi@gmail.com>
2022-11-28 08:43:36 +0100

    stack empty

---
+++ general.js
@@ -0,0 +1,8 @@
+function respTopnav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
